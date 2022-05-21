c9d5cbac5a62a9038578e64f3be9b6ddafc5f6c8
Иван <dvvn@ya.ru>
2022-05-22 01:39:35 +0300

    format.ixx -> format.h

---
+++ impl/nstd/format.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef NSTD_LIB_FORMAT
+#warning "NSTD_LIB_FORMAT not found"
+#else
+#include <version>
+
+#ifdef __cpp_lib_format
+#include <format>
+#define _FMT std
+#else
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+#include <fmt/xchar.h>
+#define _FMT fmt
+#endif
+
+namespace nstd
+{
+    using _FMT::format;
+    using _FMT::format_to;
+    using _FMT::vformat;
+
+    using _FMT::formatter;
+
+    using _FMT::make_format_args;
+    using _FMT::make_wformat_args;
+
+} // namespace nstd
+
+#undef _FMT
+#endif
